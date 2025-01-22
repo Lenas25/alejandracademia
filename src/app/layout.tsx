b3740436c2d1efbaae11354dc51adb2ce59b75b7
Lena Suarez <102402197+Lenas25@users.noreply.github.com>
2025-01-22 16:41:56 -0500

    styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import "./globals.css";
 import { Lato } from "next/font/google";
 import ProviderComp from "@/redux/provider";
+import { useEffect } from "react";
 
 const lato = Lato({
   weight: ["400", "700", "900"],
@@ -15,6 +16,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  useEffect(() => {
+    document.documentElement.classList.add('dark');
+  }, []);
+
+
   return (
     <html lang="en" className="scroll-smooth">
       <body className={lato.className}>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 import daisyui from 'daisyui';
 
 export default {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
