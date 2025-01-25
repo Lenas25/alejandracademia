01afa6de35bc2391331937f8cb28a37c931c01f9
Lena Suarez <102402197+Lenas25@users.noreply.github.com>
2025-01-25 13:43:54 -0500

    better

--- src/app/intranet/layout.tsx
+++ src/app/intranet/layout.tsx
@@ -9,22 +9,22 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    const router = useRouter();
-    
-    useEffect( () => {
-      const verificar = async () => {
-        const token = localStorage.getItem("token");
-        if (token && !isTokenExpired(token)) {
-          const user = await getMe(localStorage.getItem("token") as string);
-          const role =  user?.role === Roles.TUTOR ? Roles.ADMIN : user?.role;
-          router.push(`/intranet/${role}`);
-        }
-        router.push("/intranet");
-      }
-      verificar();
-    }, [router]);
+  const router = useRouter();
 
-  return (
-        {children}
+  useEffect(() => {
+    const verificar = async () => {
+      const token = localStorage.getItem("token");
+      if (token && !isTokenExpired(token)) {
+        const user = await getMe(localStorage.getItem("token") as string);
+        const role = user?.role === Roles.TUTOR ? Roles.ADMIN : user?.role;
+        router.push(`/intranet/${role}`);
+      }
+      router.push("/intranet");
+    };
+    verificar();
+  }, [router]);
+
+  return(
+  { children }
   );
 }
